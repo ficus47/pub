d917acfe164d28519828c21c4412a4742abc166f
ficus47 <140836421+ficus47@users.noreply.github.com>
2024-06-06 15:40:14 +0000

    zedfqrfdfs zre# Please enter the commit message for your changes. Lines starting

--- main.py
+++ main.py
@@ -1,26 +1,9 @@
 import streamlit as st
 import streamlit.components.v1 as components
-import os
 
-# Fonction pour injecter le code HTML dans la balise <head>
-def inject_html_in_head():
-    custom_head = """
-    <head>
-    <title>Custom Streamlit App</title>
-    <meta name="google-adsense-account" content="ca-pub-8075907034534804">
-    <style>
-        body {
-            background-color: lightgrey;
-        }
-    </style>
-    </head>
-    """
-    components.html(custom_head, height=0)
+adsense_code = """
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8075907034534804"
+     crossorigin="anonymous"></script>
+"""
 
-st.write(os.listdir())
-st.write("click ! ")
-
-st.write(os.listdir(os.listdir()[1]))
-st.write(os.listdir(os.listdir()[2]))
-
-st.write(os.listdir(os.listdir()[2]+"/HEAD"))
+components.html(adsense_code, height=100)
